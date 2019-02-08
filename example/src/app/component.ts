ccc87597c604dffc061e70af98fdc0f5006220b4
ruxandra <ruxandramatei0@gmail.com>
2019-02-08 13:51:36 +0200

    Data Bindings and Built-in Directives

---
+++ example/src/app/component.ts
@@ -0,0 +1,64 @@
+import { Model } from "./repository.model";
+import { ApplicationRef, Component } from "@angular/core" 
+import { Product } from "./product.model"
+
+@Component({
+    selector: "app",
+    templateUrl: "template.html"
+})
+export class ProductComponent {
+    model: Model = new Model();
+
+    constructor(ref: ApplicationRef){
+        (<any>window).appRef = ref;
+        (<any>window).model = this.model;
+    }
+    
+    // getClasses(key: number): string{
+    //     let product = this.model.getProduct(key);
+    //     return "p-2 "+(product.price < 50 ? "bg-info" : "bg-warning");
+    // }
+
+    // getClassMap(key: number): Object{
+    //     let product = this.model.getProduct(key);
+    //     return {
+    //         "tect-center bg-danger" : product.name =="Kayak",
+    //         "bg-info": product.price < 50
+    //     }
+    // }
+    // fontSizeWithUnits: string = "30px";
+    // fontSizeWithoutUnits: string = "30";
+
+    // getStyles(key: number){
+
+    //     let product = this.model.getProduct(key);
+    //     return{
+    //         fontSize: "30px",
+    //         "margin.px": 100,
+    //         color: product.price > 50 ? "red" : "green"
+    //     };
+    // }
+
+    getProductByPosition(position: number): Product{
+        return this.model.getProducts()[position];
+    }
+
+    getClassesByPosition(position: number): string {
+        let product = this.getProductByPosition(position);
+        return "p-2" + (product.price < 50 ? " bg-info": " bg-warning");
+    }
+
+    getProduct(key: number): Product {
+        return this.model.getProduct(key);
+    }
+
+    getProducts(): Product[] {
+        return this.model.getProducts();
+    }
+
+    getProductCount(): number {
+        return this.getProducts().length;
+    }
+    
+    targetName: string = "Kayak";
+}
