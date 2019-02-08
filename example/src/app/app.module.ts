9533a4a5e8904d1bc71f43a56da609f4a359f42d
ruxandra <ruxandramatei0@gmail.com>
2019-02-08 20:16:30 +0200

    Added event-binding

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductComponent } from "./component";
+import { FormsModule } from "@angular/forms"; 
 
 @NgModule({
     declarations: [ProductComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, FormsModule],
     providers: [],
     bootstrap: [ProductComponent]
 })
--- example/src/app/component.ts
+++ example/src/app/component.ts
@@ -1,78 +1,25 @@
+import { ApplicationRef, Component } from "@angular/core";
 import { Model } from "./repository.model";
-import { ApplicationRef, Component } from "@angular/core"
-import { Product } from "./product.model"
+import { Product } from "./product.model";
 
 @Component({
     selector: "app",
-    templateUrl: "template.html"
+    templateUrl:"template.html"
 })
-export class ProductComponent {
-    model: Model = new Model();
+export class ProductComponent{
+    model : Model = new Model();
 
-    constructor(ref: ApplicationRef) {
-        (<any>window).appRef = ref;
-        (<any>window).model = this.model;
-    }
-
-    // getClasses(key: number): string{
-    //     let product = this.model.getProduct(key);
-    //     return "p-2 "+(product.price < 50 ? "bg-info" : "bg-warning");
-    // }
-
-    // getClassMap(key: number): Object{
-    //     let product = this.model.getProduct(key);
-    //     return {
-    //         "tect-center bg-danger" : product.name =="Kayak",
-    //         "bg-info": product.price < 50
-    //     }
-    // }
-    // fontSizeWithUnits: string = "30px";
-    // fontSizeWithoutUnits: string = "30";
-
-    // getStyles(key: number){
-
-    //     let product = this.model.getProduct(key);
-    //     return{
-    //         fontSize: "30px",
-    //         "margin.px": 100,
-    //         color: product.price > 50 ? "red" : "green"
-    //     };
-    // }
-
-    getProductByPosition(position: number): Product {
-        return this.model.getProducts()[position];
-    }
-
-    getClassesByPosition(position: number): string {
-        let product = this.getProductByPosition(position);
-        return "p-2" + (product.price < 50 ? " bg-info" : " bg-warning");
-    }
-
-    getProduct(key: number): Product {
+    getProduct(key: number): Product{
         return this.model.getProduct(key);
     }
 
-    getProducts(): Product[] {
+    getProducts(): Product[]{
         return this.model.getProducts();
     }
 
-    getProductCount(): number {
-        console.log("getProductCount invoked");
-        return this.getProducts().length;
-    }
+    selectedProduct: string;
 
-    getKey(index: number, product: Product) {
-        return product.id;
+    getSelected(product: Product):boolean{
+        return product.name == this.selectedProduct;
     }
-
-    get nextProduct(): Product {
-        return this.model.getProducts().shift();
-    }
-
-    getProductPrice(index: number): number {
-        return Math.floor(this.getProduct(index).price);
-    }
-
-    targetName: string = "Kayak";
-    counter: number = 1;
 }
