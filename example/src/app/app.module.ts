12d10b6735eb09d16518749cdaa1893f2c8c5e43
ruxandra <ruxandramatei0@gmail.com>
2019-02-09 16:08:06 +0200

    Added Property Input

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductComponent } from "./component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms"; 
+import { PaAttrDirective } from "./attr.directive";
 
 @NgModule({
-    declarations: [ProductComponent],
+    declarations: [ProductComponent, PaAttrDirective],
     imports: [BrowserModule, FormsModule,ReactiveFormsModule],
     providers: [],
     bootstrap: [ProductComponent]
--- example/src/app/component.ts
+++ example/src/app/component.ts
@@ -22,38 +22,12 @@ export class ProductComponent {
 
     newProduct: Product = new Product();
 
-    get jsonProduct() {
-        return JSON.stringify(this.newProduct);
-    }
-
     addProduct(p: Product) {
-        console.log("New Product: " + this.jsonProduct);
+        this.model.saveProduct(p);
     }
 
-    // getValidationMessages(state: any, thingName?: string) {
-    //     let thing: string = state.path || thingName;
-    //     let messages: string[] = [];
-    //     if (state.errors) {
-    //         for (let errorName in state.errors) {
-    //             switch (errorName) {
-    //                 case "required":
-    //                     messages.push(`You must enter a ${thing}`);
-    //                     break;
-    //                 case "minlength":
-    //                     messages.push(`A ${thing} must be at least
-    //                         ${state.errors['minlength'].requiredLength}
-    //                         characters`);
-    //                     break;
-    //                 case "pattern":
-    //                     messages.push(`The ${thing} contains
-    //                          illegal characters`);
-    //                     break;
-    //             }
-    //         }
-    //     }
-    //     return messages;
-    // }
     formSubmitted: boolean = false;
+
     submitForm(form: NgForm) {
         this.formSubmitted = true;
         if (form.valid) {
@@ -64,11 +38,4 @@ export class ProductComponent {
         }
     }
 
-    // getFormValidationMessages(form: NgForm): string[]{
-    //     let messages: string[] = [];
-    //     Object.keys(form.controls).forEach(k => {
-    //         this.getValidationMessages(form.controls[k], k).forEach(m => messages.push(m));
-    //     });
-    //     return messages;
-    // }
 }
