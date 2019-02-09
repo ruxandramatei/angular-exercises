0a0004596f7dbcacfc5c5cc4db92921f1aeed258
ruxandra <ruxandramatei0@gmail.com>
2019-02-09 18:03:16 +0200

    Exporting a directive

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from "./component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms"; 
 import { PaAttrDirective } from "./attr.directive";
+import { PaModel } from "./twoway.directive";
 
 @NgModule({
-    declarations: [ProductComponent, PaAttrDirective],
+    declarations: [ProductComponent, PaAttrDirective, PaModel],
     imports: [BrowserModule, FormsModule,ReactiveFormsModule],
     providers: [],
     bootstrap: [ProductComponent]
--- example/src/app/attr.directive.ts
+++ example/src/app/attr.directive.ts
@@ -1,23 +1,46 @@
-import { Directive, ElementRef, Attribute, Input , SimpleChange} from "@angular/core";
+import { Directive, ElementRef, Attribute, Input, SimpleChange, Output, EventEmitter, HostListener, HostBinding } from "@angular/core";
+import { Product } from "./product.model";
 
 @Directive({
-    selector:"[pa-attr]",
+    selector: "[pa-attr]",
 })
-export class PaAttrDirective{
-    constructor(private element: ElementRef){}
+export class PaAttrDirective {
     @Input("pa-attr")
+    @HostBinding("class")
     bgClass: string;
-    ngOnChanges(changes: {[property: string]: SimpleChange}){
-        let change = changes["bgClass"];
-        let classList = this.element.nativeElement.classList;
 
-        if(!change.isFirstChange() && classList.contains(change.previousValue)){
-            classList.remove(change.previousValue);
-        }
-        if(!classList.contains(change.currentValue)){
-            classList.add(change.currentValue);
+    @Input("pa-product")
+    product: Product;
+
+    @Output("pa-category")
+    click = new EventEmitter<string>();
+
+    @HostListener("click")
+    triggerCustomEvent(){
+        if(this.product != null){
+            this.click.emit(this.product.category);
         }
     }
+
+    // constructor(private element: ElementRef) {
+    //     this.element.nativeElement.addEventListener("click", e => {
+    //         if (this.product != null) {
+    //             this.click.emit(this.product.category);
+    //         }
+    //     })
+    // }
+
+    // ngOnChanges(changes: { [property: string]: SimpleChange }) {
+    //     let change = changes["bgClass"];
+    //     let classList = this.element.nativeElement.classList;
+
+    //     if (!change.isFirstChange() && classList.contains(change.previousValue)) {
+    //         classList.remove(change.previousValue);
+    //     }
+    //     if (!classList.contains(change.currentValue)) {
+    //         classList.add(change.currentValue);
+    //     }
+    // }
     // ngOnInit(){
     //     this.element.nativeElement.classList.add(this.bgClass || "bg-success", "text-white");
     // }
