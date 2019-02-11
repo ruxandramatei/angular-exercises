debf6f7367bdb04216fee3148f97d4f9e5afb0af
ruxandra <ruxandramatei0@gmail.com>
2019-02-11 11:15:38 +0200

    Dealing with Collecion-Level Data Changes

--- example/src/app/component.ts
+++ example/src/app/component.ts
@@ -26,6 +26,10 @@ export class ProductComponent {
         this.model.saveProduct(p);
     }
 
+    deleteProduct(key: number){
+        this.model.deleteProduct(key);
+    }
+
     formSubmitted: boolean = false;
 
     submitForm(form: NgForm) {
