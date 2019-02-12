966199677dcefac075fd76cdedd41c194f40fc44
ruxandra <ruxandramatei0@gmail.com>
2019-02-12 21:52:57 +0200

    Added ExampleApp

---
+++ exampleApp/src/app/model/model.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { StaticDataSource } from "./static.datasource";
+import { Model } from "./repository.model";
+
+@NgModule({
+    providers: [Model, StaticDataSource]
+})
+export class ModelModule{
+    
+}
