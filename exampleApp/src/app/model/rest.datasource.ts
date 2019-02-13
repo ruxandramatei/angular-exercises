9c3b46243bb514c1699cf2ed3e03a75e0f1b20aa
ruxandra <ruxandramatei0@gmail.com>
2019-02-13 19:15:11 +0200

    Configuring Requests Headers

--- exampleApp/src/app/model/rest.datasource.ts
+++ exampleApp/src/app/model/rest.datasource.ts
@@ -1,33 +1,40 @@
 import { Injectable, Inject, InjectionToken } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Product } from "./product.model";
 
 export const REST_URL = new InjectionToken("rest_url");
 
 @Injectable()
-export class RestDataSource{
-    constructor(private http: HttpClient, @Inject(REST_URL) private url: string ){
+export class RestDataSource {
+    constructor(private http: HttpClient, @Inject(REST_URL) private url: string) {
 
     }
 
-    getData(): Observable<Product[]>{
+    getData(): Observable<Product[]> {
         return this.http.jsonp<Product[]>(this.url, "callback");
     }
 
-    saveProduct(product: Product): Observable<Product>{
+    saveProduct(product: Product): Observable<Product> {
         return this.sendRequest<Product>("POST", this.url, product);
     }
 
-    updateProduct(product: Product): Observable<Product>{
-        return this.sendRequest<Product>("GET",`${this.url}/${product.id}`, product);
+    updateProduct(product: Product): Observable<Product> {
+        return this.sendRequest<Product>("GET", `${this.url}/${product.id}`, product);
     }
 
-    deleteProduct(id: number): Observable<Product>{
-        return this.sendRequest<Product>("GET",`${this.url}/${id}`);
+    deleteProduct(id: number): Observable<Product> {
+        return this.sendRequest<Product>("GET", `${this.url}/${id}`);
     }
 
-    private sendRequest<T>(verb: string, url: string, body?: Product): Observable<T>{
-        return this.http.request<T>(verb, url, {body: body});
+    private sendRequest<T>(verb: string, url: string, body?: Product): Observable<T> {
+        let myHeaders = new HttpHeaders();
+        myHeaders = myHeaders.set("Access-Key", "<secret>");
+        myHeaders = myHeaders.set("Application-Names", ["exampleApp", "proAngular"]);
+        return this.http.request<T>(verb, url,
+            {
+                body: body,
+                headers: myHeaders
+            });
     }
 }
