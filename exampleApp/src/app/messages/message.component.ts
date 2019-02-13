d60acf6548ea7f694b08fab0e27af446a330696a
ruxandra <ruxandramatei0@gmail.com>
2019-02-13 12:33:26 +0200

    Using provider factory

--- exampleApp/src/app/messages/message.component.ts
+++ exampleApp/src/app/messages/message.component.ts
@@ -1,6 +1,7 @@
 import { Component }  from "@angular/core";
 import { MessageService } from "./message.service";
 import { Message } from "./message.model";
+import { Observable } from "rxjs";
 
 @Component({
     selector: "paMessages",
@@ -9,6 +10,6 @@ import { Message } from "./message.model";
 export class MessageComponent{
     lastMessage: Message;
     constructor( messageService: MessageService){
-        messageService.registerMessageHandler(m => this.lastMessage = m);
+       messageService.messages.subscribe(m => this.lastMessage = m);
     }
 }
