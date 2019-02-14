9ad95a40401c047bf7d81167dbe7415e3b5fa785
ruxandra <ruxandramatei0@gmail.com>
2019-02-14 19:20:58 +0200

    Added OnDemand components

--- exampleApp/src/app/ondemand/second.component.ts
+++ exampleApp/src/app/ondemand/second.component.ts
@@ -4,4 +4,6 @@ import { Component } from "@angular/core";
     selector: "second",
     template: `<div class="bg-info text-white p-2">Second Component</div>`
 })
-export class SecondComponent { }
+export class SecondComponent { 
+    
+}
