c55a637422bf578991badc78771b851f5ad157d7
ruxandra <ruxandramatei0@gmail.com>
2019-02-15 14:16:49 +0200

    Added CSS Framework Styles

--- exampleApp/src/app/core/table.animations.ts
+++ exampleApp/src/app/core/table.animations.ts
@@ -7,11 +7,14 @@ const commonStyles = {
 };
 
 export const HighlightTrigger = trigger("rowHighlight", [
-    state("selected", style(getStylesFromClasses(["bg-success", "h2"]))),
-    state("notselected", style(getStylesFromClasses("bg-info"))),
+    state("selected", style(
+        getStylesFromClasses(["bg-success", "h2"])
+    )),
+    state("notselected", style(
+        getStylesFromClasses("bg-info")
+    )),
     state("void", style({
         transform: "translateX(-50%)"
-        //transform: "scale(1.1, 1.1) rotate(10deg)"
     })),
     transition("* => notselected", animate("200ms")),
     transition("* => selected", animate("400ms 200ms ease-in")),
